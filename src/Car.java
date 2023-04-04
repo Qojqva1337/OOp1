5d0445036b6ce8ef65752a79a0f64fabf89d49b8
Qojqva <artursuper10@gmail.com>
2023-04-04 13:40:33 +0300

    OOp1

---
+++ src/Car.java
@@ -0,0 +1,77 @@
+public class Car {
+    public String brand;
+    public String model;
+    public double engineVolume;
+    public String color;
+    public int year;
+    public String country;
+
+    public Car(String brand, String model, double engineVolume, String color, int year, String country) {
+        this.brand = brand;
+        this.model = model;
+        this.engineVolume = engineVolume;
+        this.color = color;
+        this.year = year;
+        this.country = country;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public double getEngineVolume() {
+        return engineVolume;
+    }
+
+    public void setEngineVolume(double engineVolume) {
+        this.engineVolume = engineVolume;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "brand='" + brand + '\'' +
+                ", model='" + model + '\'' +
+                ", engineVolume=" + engineVolume +
+                ", color='" + color + '\'' +
+                ", year=" + year +
+                ", country='" + country + '\'' +
+                '}';
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,29 @@
+public class Main {
+    public static void main(String[] args) {
+        Car Lada = new Car(" Lada", " Granta ", 1.7, " Желтый ", 2015, " Russia ");
+
+        System.out.println(Lada);
+
+        Car Audi = new Car(" Audi "," a8 ", 1.7," Желтый ", 2015,  " Russia ");
+
+        System.out.println(Audi);
+
+        Car BMW = new Car("BMW", "z8", 3.0, " Черный ", 2021, "Germany");
+
+        System.out.println(BMW);
+
+        Car KIA = new Car(" KIA ", " Sportage ", 2.4," Булеый ", 2018, " Корея ");
+
+        System.out.println(KIA);
+
+        Car Hyundai = new Car("Hyundai"," Avante ", 1.6, " оранжевый ", 2016, " Корея ");
+
+
+        System.out.println(Hyundai);
+
+
+
+
+    }
+
+}
